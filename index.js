dbd3d86a5534aa8ea71f500d26d9d52eac088f68
unknown <ayondevs.94b@gmail.com>
2026-06-11 22:09:42 +0530

    Backend setup and creating the server

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const cors = require ("cors")
 const port = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello WorkOrbit Server!");
 });
-
-
 
 const uri = process.env.MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,21 +28,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const database = client.db("WorkOrbitDB");
+    const jobCollection = database.collection("jobs");
+
+    app.post("/jobs", async (req, res) => {
+      try {
+        const job = req.body;
+
+        const result = await jobCollection.insertOne(job);
+
+        res.status(201).send(result);
+      } catch (error) {
+        console.error(error);
+
+        res.status(500).send({
+          success: false,
+          message: "Failed to create job",
+        });
+      }
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
