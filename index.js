37d15888db1bfc7cc85ddf146aba9c70ae1462f4
unknown <ayondevs.94b@gmail.com>
2026-06-13 00:36:43 +0530

    Implement company backend CRUD foundation and ImageBB integration

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const axios = require("axios");
+const FormData = require("form-data");
 require("dotenv").config();
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -28,10 +31,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+    const upload = multer({
+      storage: multer.memoryStorage(),
+    });
     const database = client.db("WorkOrbitDB");
     const jobCollection = database.collection("jobs");
     const companyCollection = database.collection("companies");
+
+    app.post("/api/company", async (req, res) => {
+      try {
+        const company = req.body;
+
+        const result = await companyCollection.insertOne(company);
+
+        res.status(201).send(result);
+      } catch (error) {
+        console.error(error);
+
+        res.status(500).send({
+          success: false,
+          message: "Failed to create company",
+        });
+      }
+    });
+
+
 
     app.post("/api/jobs", async (req, res) => {
       try {
@@ -46,6 +70,39 @@ async function run() {
         res.status(500).send({
           success: false,
           message: "Failed to create job",
+        });
+      }
+    });
+
+    // LOGO upload post method
+    app.post("/api/upload-logo", upload.single("image"), async (req, res) => {
+      try {
+        const apiKey = process.env.IMGBB_API_KEY;
+
+        const formData = new FormData();
+
+        formData.append("image", req.file.buffer.toString("base64"));
+
+        const response = await axios.post(
+          `https://api.imgbb.com/1/upload?key=${apiKey}`,
+          formData,
+          {
+            headers: formData.getHeaders(),
+          },
+        );
+
+        const logoUrl = response.data.data.url;
+
+        res.send({
+          success: true,
+          logoUrl,
+        });
+      } catch (error) {
+        console.error(error);
+
+        res.status(500).send({
+          success: false,
+          message: "Image upload failed",
         });
       }
     });
@@ -67,13 +124,36 @@ async function run() {
       }
     });
 
-//  export const getCompanyJobs = async (companyId) => {
-//    const res = await fetch(`${baseURL}/api/jobs/company/${companyId}`, {
-//      cache: "no-store",
-//    });
+    //  export const getCompanyJobs = async (companyId) => {
+    //    const res = await fetch(`${baseURL}/api/jobs/company/${companyId}`, {
+    //      cache: "no-store",
+    //    });
 
-//    return res.json();
-//  };
+    //    return res.json();
+    //  };
+
+    app.get("/api/company/:companyId", async (req, res) => {
+      try {
+        const companyId = req.params.companyId;
+
+        const company = await companyCollection.findOne({
+          companyId,
+        });
+
+        return res.status(200).json({
+          success: true,
+          company,
+        });
+      } catch (error) {
+        console.error(error);
+
+        res.status(500).send({
+          success: false,
+          message: "Failed to fetch company",
+        });
+      }
+    });
+
 
     await client.db("admin").command({ ping: 1 });
     console.log(
